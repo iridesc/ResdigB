f02b4bae649915a243b0b7a77e692a56062f7acf
iridesc <lielang6@live.com>
2019-06-20 00:52:27 +0800

    一键收集静态文件 并压缩js文件为core.js

--- Encryption/cmd.sh
+++ Encryption/cmd.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-pwd
-echo del core.js...
-ls ../resdig/static/resdig/js
-echo 'cp all js to here...'
-cp ../resdig/static/resdig/js/*js ../Encrypthon
-echo 'compileing...'
-java -jar compiler.jar --js **.js   --js_output_file core.js
+python3 ../manage.py collectstatic
 
+ls ../static/resdig/js/
+
+echo 'compileing...'
+java -jar compiler.jar --js ../static/resdig/js/*.js   --js_output_file core.js
+rm ../static/resdig/js/*
+cp core.js ../static/resdig/js/
 echo 'done!'
